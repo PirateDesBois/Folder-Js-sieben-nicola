9cbadababea6c47f2caf154665c79d682c8e096b
Nicola <sieben.nicola@gmail.com>
2021-12-07 11:06:41 +0100

    javascript

---
+++ variables/exercice2/javascript_les_variables_exo_02/public/js/main.js
@@ -0,0 +1,28 @@
+var variable =("J’aime les tartines et j’en mange");
+console.log(variable);
+
+var other =("4 par jours");
+console.log(variable + other);
+
+let troiseme = 5;
+let quatrieme = 35;
+console.log(troiseme + quatrieme);
+
+console.log(variable +troiseme);
+
+let first = ("salut tout le monde");
+let second =("au revoir");
+
+first = second
+console.log(first);
+
+let number1 = 4;
+let number2 =("6");
+
+number1 > number2
+number1 < number2
+number1 == number2
+number1 != number2
+number1 === number2
+number1 !== number2
+console.log(number1)
