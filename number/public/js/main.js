e49f284e998d7651c39289127be15fd0289bc427
Nicola <sieben.nicola@gmail.com>
2021-12-07 16:30:18 +0100

    javascript

--- number/public/js/main.js
+++ number/public/js/main.js
@@ -8,4 +8,6 @@ console.log(Math.random(0,1));
 console.log(Math.floor(Math.random()*100) + 1);
 console.log(Math.pow(8, 2));
 console.log(Math.sqrt(9));
-
+console.log(Math.abs(1));
+console.log(Math.max(-2,1000,8,57));
+console.log(Math.min(-2, 1000, 8, 57));
