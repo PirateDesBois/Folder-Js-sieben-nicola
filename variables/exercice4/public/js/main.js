b99f76e6cb9ef638c02977e627efcc7b6f048867
Nicola <sieben.nicola@gmail.com>
2021-12-07 13:58:38 +0100

    javascript

--- variables/exercice4/public/js/main.js
+++ variables/exercice4/public/js/main.js
@@ -1,19 +1,17 @@
-//let reponse = prompt("your name","babaorhum")
-
-//if( reponse == null ){
-//  alert("tu crains");
-//   alert("Bonjour " + reponse + ", ça roule ?");
-//}
+let ask = prompt("your name",)
+alert("Bonjour " + ask + ", ça roule ?");
 
 
 
-var question = prompt("quel age as-tu?")
-let reponse =question
-alert("tu as " + reponse +"ans")
 
-var question2 = prompt("quel est ton nom")
-var question3 = prompt("quel est ton prenom")
-let réponse1 =question2
-let réponse2 =question3
+var question1 = prompt("quel age as-tu?")
+alert ("tu as " + question1 +"ans")
 
-alert("tes noms et prénoms sont:"+ réponse1.toUpperCase()+"et"+réponse2);
+
+
+
+var nom = prompt("quel est ton nom")
+var prenom = prompt("quel est ton prenom")
+
+
+alert("tes noms et prénoms sont:"+ nom.toUpperCase()+" et "+prenom);
